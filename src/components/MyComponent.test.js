830c9db286e222dddbf0e8f00ce8d3700fbb11ec
Fang Lu Lim <fanglulim96@gmail.com>
2022-04-11 19:14:21 -0700

    first commit

---
+++ src/components/MyComponent.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MyComponent from './MyComponent';
+
+describe("MyComponent", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(<MyComponent />);
+  });
+  it("should render initial layout", () => {
+    // when 
+    const component = shallow(<MyComponent />);
+    // then
+    expect(component.getElements()).toMatchSnapshot();
+  });
+});
