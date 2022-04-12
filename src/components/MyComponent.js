ce2dca6d2ca218ee5898a8806d98a0ca123b9920
Fang Lu Lim <fanglulim96@gmail.com>
2022-04-11 20:05:33 -0700

    complete

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default class MyComponent extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <input
+          onChange={(event) => {
+            this.setState({ input: event.target.value })}}
+          type="text"/>
+    </div>
+    );
   }
 }
--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import MyComponent from './MyComponent';
+import React from "react";
+import { shallow } from "enzyme";
+import MyComponent from "./MyComponent";
 
 describe("MyComponent", () => {
   it("should render correctly", () => {
     const wrapper = shallow(<MyComponent />);
   });
   it("should render initial layout", () => {
-    // when 
+    // when
     const component = shallow(<MyComponent />);
     // then
-    expect(component.getElements()).toMatchSnapshot();
+    expect(component.getElements()).toMatchSnapshot(); //best practice to test this first
+  });
+  it("should create an entry in component state", () => {
+    // given
+    const component = shallow(<MyComponent />);
+    const form = component.find("input");
+    // when
+    form.props().onChange({
+      target: {
+        name: "myName",
+        value: "myValue",
+      },
+    });
+    //then
+    expect(component.state("input")).toBeDefined();
+  });
+  it("should create an entry in component state with the event value", () => {
+    // given
+    const component = shallow(<MyComponent />);
+    const form = component.find("input");
+    // when
+    form.props().onChange({
+      target: {
+        name: "myName",
+        value: "myValue",
+      },
+    });
+    //then
+    expect(component.state("input")).toEqual("myValue");
   });
 });
